f67b63c1ce88945c4febfbc5d53bbe614bf9802c
SunnyQjm <qjm253@pku.edu.cn>
2020-02-23 22:13:26 +0800

    删除不必要文件

--- main.cpp
+++ main.cpp
@@ -4,17 +4,17 @@
 
 int main()
 {
-	srand(245);
-	RandomGraph g;
+    srand(245);
+    RandomGraph g;
 //	g.print();
-	//system("pause");
-	int successNum = 0;
-	int totalNum = 10000;
+    //system("pause");
+    int successNum = 0;
+    int totalNum = 10000;
     srand((unsigned)time(NULL));
     for (int i = 0; i < totalNum; i++) {
-		if(g.randomRoutingTest()) {
-		    successNum++;
-		}
-	}
-	cout << "路由成功率：" << successNum * 100.0 / totalNum << "%" << endl;
+        if(g.randomRoutingTest()) {
+            successNum++;
+        }
+    }
+    cout << "路由成功率：" << successNum * 100.0 / totalNum << "%" << endl;
 }
